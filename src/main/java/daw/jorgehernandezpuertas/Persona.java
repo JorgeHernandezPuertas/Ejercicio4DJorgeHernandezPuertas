25598ab7f31c0a9ab0e923211831fe8aa4b087c2
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2022-11-23 14:06:44 +0100

    Termino el ejercicio

---
+++ src/main/java/daw/jorgehernandezpuertas/Persona.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw.jorgehernandezpuertas;
+
+/**
+ *
+ * @author jorge
+ */
+public class Persona {
+
+    // Atributos de la persona
+    private String nombre;
+    private int edad;
+
+    // Constructores
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        if (edad < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.edad = edad;
+        }
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        if (edad < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.edad = edad;
+        }
+    }
+    
+    // Método toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona{");
+        sb.append("nombre=").append(nombre);
+        sb.append(", edad=").append(edad);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    // Método llamar
+    public void llamar(Animal pet) {
+        pet.despertar();
+    }
+    
+    //Método alimentar
+    public void alimentar(Animal pet, double cantidadGramos) {
+        pet.comer(cantidadGramos);
+    }
+    
+    // Método jugar
+    public void jugar(Animal pet, int cantidadMinutos) {
+        cantidadMinutos = Math.abs(cantidadMinutos);
+        if (cantidadMinutos > 180) {
+            pet.jugar(180); // 180 es la cantidad máxima
+            throw new IllegalArgumentException();
+        } else {
+            pet.jugar(cantidadMinutos);
+        }
+    }
+}
---
+++ src/main/java/daw/jorgehernandezpuertas/Prueba.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw.jorgehernandezpuertas;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author jorge
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        // Creo dos objetos animal
+        Animal a2 = new Animal();
+        Animal a1 = new Animal(LocalDate.of(2006, 2, 12),
+                "milo", "perro", 3430, "durmiendo");
+
+        // Creo dos personas
+        Persona p1 = new Persona("Jorge", 22);
+        Persona p2 = new Persona("Cristina", 22);
+
+        // Imprimo por consola a1 y a2
+        System.out.println(a1);
+        System.out.println(a2);
+
+        // Copio a2 en a3
+        Animal a3 = Animal.clonar(a2);
+
+        // p1 despierta a todos los animales
+        p1.llamar(a1);
+        p1.llamar(a2);
+        p1.llamar(a3);
+
+        // p2 juega con a2 durante 120 minutos
+        p2.jugar(a2, 120);
+
+        // p1 alimenta a a1 1000 gramos e imprimo su peso
+        p1.alimentar(a1, 1000);
+        System.out.println("a1 pesa " + a1.getPeso() + " gramos");
+
+        // p1 juega con a1 durante 200 minutos (no se puede y jugará 180 minutos)
+        try {
+            p1.jugar(a1, 200);
+        } catch (IllegalArgumentException iae) {
+            
+        }
+        System.out.println("a1 pesa " + a1.getPeso() + " gramos");
+        
+    }
+}
